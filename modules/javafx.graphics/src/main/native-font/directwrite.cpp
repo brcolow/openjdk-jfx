0ef37d960563bf988c57fce0a52fcb31a8e9f842
Michael Ennen <mike.ennen@gmail.com>
2018-08-10 23:54:54 -0700

    z

--- modules/javafx.graphics/src/main/native-font/directwrite.cpp
+++ modules/javafx.graphics/src/main/native-font/directwrite.cpp
@@ -856,17 +856,11 @@ JNIEXPORT jlong JNICALL OS_NATIVE(_1WICCreateImagingFactory)
      * Note: This method is called by DWFactory a single time.
      */
     IWICImagingFactory* result = NULL;
-    HRESULT hr = CoInitializeEx(NULL, COINIT_MULTITHREADED); {
-        /* This means COM has been initialized with a different concurrency model.
-        * This should never happen. */
-        if (hr == RPC_E_CHANGED_MODE) return NULL;
-        hr = CoCreateInstance(CLSID_WICImagingFactory1, NULL, CLSCTX_INPROC_SERVER, __uuidof(IWICImagingFactory), reinterpret_cast<void**>(&result));
-        if (result == NULL) {
-            fprintf(stderr, "result was NULL");
-        }
-    }
-    if (SUCCEEDED(hr)) {
-        CoUninitialize();
+    /* This means COM has been initialized with a different concurrency model.
+    * This should never happen. */
+    hr = CoCreateInstance(CLSID_WICImagingFactory1, NULL, CLSCTX_INPROC_SERVER, __uuidof(IWICImagingFactory), reinterpret_cast<void**>(&result));
+    if (result == NULL) {
+        fprintf(stderr, "result was NULL");
     }
     return SUCCEEDED(hr) ? (jlong)result : NULL;
 }
